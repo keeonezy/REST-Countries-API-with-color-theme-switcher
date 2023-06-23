54252a73243c0ae946453d582a1138922a652bc1
Alexander Malukhin <keeonenew@yandex.ru>
2023-06-23 21:28:57 +0300

    feat: add search flag name

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const flagTemplate = document.getElementById("flags");
 // flagsUl.appendChild(flagTemplate.content.cloneNode(true));
 // flagsUl.appendChild(flagTemplate.content.cloneNode(true));
 
-function Flags() {
+function FlagsAll() {
     fetch(`https://restcountries.com/v3.1/all`)
         .then(res => res.json())
         .then((data) => {
@@ -24,4 +24,32 @@ function Flags() {
         });
 }
 
-Flags()
+// FlagsAll()
+
+
+
+
+const searchInput = document.querySelector(".flags__input");
+
+function searchFlagName() {
+
+    fetch(`https://restcountries.com/v3.1/name/${searchInput.value}`)
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
+            data.forEach(element => {
+                flagTemplate.content.querySelector('.flags__image').src = `${element.flags.svg}`;
+                flagTemplate.content.querySelector('.flags__title').textContent = `${element.name.common}`;
+                flagTemplate.content.querySelector('.text_population').textContent = `${element.population}`;
+                flagTemplate.content.querySelector('.text_region').textContent = `${element.region}`;
+                flagTemplate.content.querySelector('.text_capital').textContent = `${element.capital}`;
+                flagsUl.appendChild(flagTemplate.content.cloneNode(true));
+            });
+
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+searchInput.addEventListener("change", searchFlagName);
