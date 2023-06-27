4ffe85201fabb21535fde7180c5d04bb38fc3712
Alexander Malukhin <keeonenew@yandex.ru>
2023-06-27 16:32:38 +0300

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -8,57 +8,57 @@ const regionTextApi = document.getElementsByClassName("text_region");
 const countryTextApi = document.getElementsByClassName("flags__title");
 
 async function getCountryAll() {
-    await fetch(`https://restcountries.com/v3.1/all`)
-        .then(res => res.json())
-        .then((data) => {
-            console.log(data);
+  await fetch(`https://restcountries.com/v3.1/all`)
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data);
 
-            data.forEach(element => {
-                showCountry(element);
-            });
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+      data.forEach(element => {
+        showCountry(element);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 getCountryAll()
 
 function showCountry(data) {
-    flagTemplate.content.querySelector('.flags__image').src = `${data.flags.svg}`;
-    flagTemplate.content.querySelector('.flags__title').textContent = `${data.name.common}`;
-    flagTemplate.content.querySelector('.text_population').textContent = `${data.population}`;
-    flagTemplate.content.querySelector('.text_region').textContent = `${data.region}`;
-    flagTemplate.content.querySelector('.text_capital').textContent = `${data.capital}`;
-    flagsUl.appendChild(flagTemplate.content.cloneNode(true));
+  flagTemplate.content.querySelector('.flags__image').src = `${data.flags.svg}`;
+  flagTemplate.content.querySelector('.flags__title').textContent = `${data.name.common}`;
+  flagTemplate.content.querySelector('.text_population').textContent = `${data.population}`;
+  flagTemplate.content.querySelector('.text_region').textContent = `${data.region}`;
+  flagTemplate.content.querySelector('.text_capital').textContent = `${data.capital}`;
+  flagsUl.appendChild(flagTemplate.content.cloneNode(true));
 }
 
 regionText.forEach(element => {
-    element.addEventListener("click", () => {
-        console.log(element)
-        Array.from(regionTextApi).forEach(elem => {
-            console.log(elem.innerText)
-            if (elem.innerText.includes(element.innerText) || element.innerText == "All") {
-                elem.parentElement.parentElement.style.display = "block";
-            } else {
-                elem.parentElement.parentElement.style.display = "none";
-            }
-        })
+  element.addEventListener("click", () => {
+    console.log(element)
+    Array.from(regionTextApi).forEach(elem => {
+      console.log(elem.innerText)
+      if (elem.innerText.includes(element.innerText) || element.innerText == "All") {
+        elem.parentElement.parentElement.style.display = "block";
+      } else {
+        elem.parentElement.parentElement.style.display = "none";
+      }
     })
+  })
 });
 
 const searchInput = document.querySelector(".flags__input");
 
 searchInput.addEventListener("input", () => {
-    console.log(searchInput.value.toLowerCase())
-    Array.from(countryTextApi).forEach(elem => {
-        console.log(elem.innerText.toLowerCase().includes(searchInput.value.toLowerCase()))
-        if (elem.innerText.toLowerCase().includes(searchInput.value.toLowerCase())) {
-            elem.parentElement.style.display = "grid";
-        } else {
-            elem.parentElement.style.display = "none";
-        }
-    });
+  console.log(searchInput.value.toLowerCase())
+  Array.from(countryTextApi).forEach(elem => {
+    console.log(elem.innerText.toLowerCase().includes(searchInput.value.toLowerCase()))
+    if (elem.innerText.toLowerCase().includes(searchInput.value.toLowerCase())) {
+      elem.parentElement.style.display = "grid";
+    } else {
+      elem.parentElement.style.display = "none";
+    }
+  });
 })
 
 
@@ -68,11 +68,11 @@ const toggleTheme = document.querySelector(".header__group-theme");
 
 // Переключаем на темный и светлый режим
 toggleTheme.addEventListener("click", () => {
-    const colorScheme = document.documentElement.getAttribute("data-color-scheme");
-    document.documentElement.setAttribute("data-color-scheme", colorScheme === "default" ? "dark" : "default");
+  const colorScheme = document.documentElement.getAttribute("data-color-scheme");
+  document.documentElement.setAttribute("data-color-scheme", colorScheme === "default" ? "dark" : "default");
 })
 
 
 dropButton.addEventListener("click", () => {
-    dropList.classList.toggle("flags_opened");
+  dropList.classList.toggle("flags_opened");
 })
